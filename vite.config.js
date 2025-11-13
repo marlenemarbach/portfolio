84a5e61b3459974c9841ce848e714d951f19fd34
Marlene <marlene.goedecke@ui.city>
2025-11-13 10:52:12 +0100

    use static html intead of js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,27 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   appType: "mpa", // multi-page app
+  plugins: [
+    {
+      name: "html-hot-reload",
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith(".html")) {
+          server.ws.send({
+            type: "full-reload",
+            path: "*",
+          });
+        }
+      },
+    },
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      overlay: true,
+    },
+  },
   build: {
     rollupOptions: {
       input: {
